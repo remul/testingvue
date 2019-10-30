fec54edfd615b8af24b6dc62c502e1a91982f3c2
remul <fabio112@live.de>
2019-10-30 16:35:19 +0100

    wip

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <span class="count" v-text="count"></span>
-    <button @click="count++">Increment</button>
+    <button @click="count++" class="increment">Increment</button>
+    <button @click="count > 0 && count--" class="decrement" v-show="count > 0">Decrement</button>
   </div>
 </template>
 
--- test/counter.spec.js
+++ test/counter.spec.js
@@ -1,8 +1,8 @@
-import { mount } from "@vue/test-utils";
+import {mount} from "@vue/test-utils";
 import Counter from '../src/components/Counter';
 import expect from 'expect';
 
-describe ('Counter', () => {
+describe('Counter', () => {
   let wrapper;
 
   beforeEach(() => {
@@ -13,12 +13,30 @@ describe ('Counter', () => {
     expect(wrapper.vm.count).toBe(0);
   });
 
-  it('increments the count when the button is clicked', () => {
+  it('increments the count when the increment button is clicked', () => {
     expect(wrapper.vm.count).toBe(0);
 
-    wrapper.find('button').trigger('click');
+    wrapper.find('.increment').trigger('click');
 
     expect(wrapper.vm.count).toBe(1);
+  });
+
+  it('decrements the count when the decrement button is clicked', () => {
+    wrapper.setData({count: 5});
+
+    wrapper.find('.decrement').trigger('click'); // 4
+
+    expect(wrapper.vm.count).toBe(4);
+  });
+
+  it('never goes below 0', () => {
+    expect(wrapper.vm.count).toBe(0);
+
+    expect(wrapper.find('.decrement').element.style.display).toBe('none');
+
+    wrapper.find('.increment').trigger('click'); // 1
+
+    expect(wrapper.find('.decrement').element.style.display).toBe('');
   });
 
   it('presents the current count', () => {
